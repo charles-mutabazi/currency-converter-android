cb60eced9eca5530d5eaf6a14c7d0999947b0234
charles.mutabazi <charl.mutabazi@gmail.com>
2023-02-14 11:21:43 +0200

    add constraint of network must be connected to perform worker

--- app/src/main/java/com/paypay/xchange_challenge/CurrencyXchangeApplication.kt
+++ app/src/main/java/com/paypay/xchange_challenge/CurrencyXchangeApplication.kt
@@ -5,9 +5,12 @@
 package com.paypay.xchange_challenge
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import com.paypay.xchange_challenge.di.appModule
+import com.paypay.xchange_challenge.util.REFRESH_INTERVAL
 import com.paypay.xchange_challenge.worker.SyncWorker
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.workmanager.koin.workManagerFactory
@@ -28,8 +31,14 @@ class CurrencyXchangeApplication : Application(), KoinComponent {
 
     //schedule work to sync data every 30 minutes
     private fun scheduleWork() {
+        //requires network connection to perform work
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
         val workRequest = PeriodicWorkRequest
-            .Builder(SyncWorker::class.java, 15, TimeUnit.MINUTES)
+            .Builder(SyncWorker::class.java, REFRESH_INTERVAL, TimeUnit.MINUTES)
+            .setConstraints(constraints)
             .build()
 
         WorkManager.getInstance(applicationContext).enqueue(workRequest)
